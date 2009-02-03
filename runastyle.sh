402f9ae7a95d6004320a6f5543f5f1a3fa9c1ae1
Amorilia <amorilia@users.sourceforge.net>
2009-02-03 16:11:50 +0000

    astyle runner now no longer ignores cpp files

--- runastyle.sh
+++ runastyle.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find . -name "*.cpp" -or -name "*.hpp" -exec astyle --style=kr -t {} \;
+find . \( -name "*.cpp" -or -name "*.hpp" \) -exec astyle --style=kr -t {} \;
 find . -name "*.orig" -exec rm {} \;
