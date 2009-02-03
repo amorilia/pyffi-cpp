23dd654d1a7dde09732ed2cd13d250a0c7a6fa94
Amorilia <amorilia@users.sourceforge.net>
2009-02-03 16:15:20 +0000

    new Args class + tests

---
+++ include/pyffi/object_models/args.hpp
@@ -0,0 +1,97 @@
+/*
+
+Copyright (c) 2007-2008, Python File Format Interface
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above
+     copyright notice, this list of conditions and the following
+     disclaimer in the documentation and/or other materials provided
+     with the distribution.
+
+   * Neither the name of the Python File Format Interface
+     project nor the names of its contributors may be used to endorse
+     or promote products derived from this software without specific
+     prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#ifndef PYFFI_ARGS_HPP_INCLUDED
+#define PYFFI_ARGS_HPP_INCLUDED
+
+// unordered_map introduced in boost 1.36.0
+#if BOOST_VERSION >= 013600
+#include <boost/unordered_map.hpp>
+#else
+#include <map>
+#endif
+
+#include "pyffi/exceptions.hpp"
+#include "pyffi/object_models/object.hpp"
+
+namespace pyffi {
+
+/*!
+ * A std::map<std::string, Object> variant with an interface that
+ * matches the rest of the library.
+ */
+class Args {
+public:
+	//! Default constructor.
+	Args() : m_map() {};
+	//! Copy constructor.
+	Args(const Args & args) : m_map(args.m_map) {};
+	//! Constructor to set the type and the value.
+	template<typename ValueType> void add(const std::string & name, const ValueType & value) {
+		// if name not found, add object to the map
+		if (m_map.find(name) == m_map.end()) {
+			m_map[name] = Object(value);
+		} else {
+			throw value_error("'" + name + "' already added to arguments.");
+		};
+	};
+	//! Get reference to value stored in the object.
+	template<typename ValueType> ValueType & get(const std::string & name) {
+		try {
+			return m_map[name].get<ValueType>();
+		} catch (const type_error &) {
+			// if there is no name key, we signal this as a key error
+			// explanation: if there is no name key, then the above code would
+			// create an Object() and return that, however,
+			// the default Object constructor throws a type_error
+			// because objects must have a value
+			throw key_error("no argument with key '" + name + "'");
+		};
+	};
+private:
+// unordered_map introduced in boost 1.36.0
+#if BOOST_VERSION >= 013600
+	typedef boost::unordered_map<std::string, Object> ArgsMap;
+#else
+	typedef std::map<std::string, Object> ArgsMap;
+#endif
+	ArgsMap m_map;
+}; // class Args
+
+}; // namespace pyffi
+
+#endif
---
+++ test/object_models/args_test.cpp
@@ -0,0 +1,119 @@
+/*
+
+Copyright (c) 2007-2008, Python File Format Interface
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above
+     copyright notice, this list of conditions and the following
+     disclaimer in the documentation and/or other materials provided
+     with the distribution.
+
+   * Neither the name of the Python File Format Interface
+     project nor the names of its contributors may be used to endorse
+     or promote products derived from this software without specific
+     prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE ArgsTest
+#include <boost/test/unit_test.hpp>
+
+#include "pyffi/object_models/args.hpp"
+#include "pyffi/exceptions.hpp"
+
+using namespace pyffi;
+
+BOOST_AUTO_TEST_CASE(add_test) {
+	Args args;
+
+	// add arguments of various types
+	args.add<int>("arg1", 5);
+	args.add<char>("arg2", 'y');
+	args.add<std::string>("arg3", "Hello world!");
+
+	BOOST_CHECK_EQUAL(args.get<int>("arg1"), 5);
+	BOOST_CHECK_EQUAL(args.get<char>("arg2"), 'y');
+	BOOST_CHECK_EQUAL(args.get<std::string>("arg3"), std::string("Hello world!"));
+
+	// check that argument cannot be added again
+	BOOST_CHECK_THROW(args.add<int>("arg1", 10), value_error);
+	BOOST_CHECK_THROW(args.add<int>("arg2", 10), value_error);
+	BOOST_CHECK_THROW(args.add<int>("arg3", 10), value_error);
+}
+
+BOOST_AUTO_TEST_CASE(constructor_test) {
+	// test default constructor
+	BOOST_CHECK_NO_THROW(Args());
+
+	// test copy constructor
+	Args args;
+	args.add<int>("arg1", 5);
+	args.add<char>("arg2", 'y');
+	args.add<std::string>("arg3", "Hello world!");
+	Args args2(args);
+	BOOST_CHECK_EQUAL(args2.get<int>("arg1"), 5);
+	BOOST_CHECK_EQUAL(args2.get<char>("arg2"), 'y');
+	BOOST_CHECK_EQUAL(args2.get<std::string>("arg3"), std::string("Hello world!"));
+
+	// test that copy constructor makes a deep copy
+	args.get<int>("arg1") = 2;
+	BOOST_CHECK_EQUAL(args.get<int>("arg1"), 2);
+	BOOST_CHECK_EQUAL(args2.get<int>("arg1"), 5);
+}
+
+BOOST_AUTO_TEST_CASE(get_test) {
+	// set up arguments
+	Args args;
+	args.add<char>("char1", 's');
+	args.add<char>("char2", 'f');
+	args.add<bool>("ic", true);
+
+	// check that type casts work as expected
+	BOOST_CHECK_THROW(args.get<int>("char1"), type_error);
+	BOOST_CHECK_NO_THROW(args.get<char>("char1"));
+	BOOST_CHECK_THROW(args.get<std::string>("char1"), type_error);
+	BOOST_CHECK_THROW(args.get<bool>("char1"), type_error);
+
+	BOOST_CHECK_THROW(args.get<int>("char2"), type_error);
+	BOOST_CHECK_NO_THROW(args.get<char>("char2"));
+	BOOST_CHECK_THROW(args.get<std::string>("char2"), type_error);
+	BOOST_CHECK_THROW(args.get<bool>("char2"), type_error);
+
+	BOOST_CHECK_THROW(args.get<int>("ic"), type_error);
+	BOOST_CHECK_THROW(args.get<char>("ic"), type_error);
+	BOOST_CHECK_THROW(args.get<std::string>("ic"), type_error);
+	BOOST_CHECK_NO_THROW(args.get<bool>("ic"));
+
+	// check assignment via get
+	args.get<char>("char2") = 'a';
+	BOOST_CHECK_EQUAL(args.get<char>("char1"), 's');
+	BOOST_CHECK_EQUAL(args.get<char>("char2"), 'a');
+	BOOST_CHECK_EQUAL(args.get<char>("ic"), true);
+
+	args.get<char>("char1") = 'a';
+	args.get<char>("ic") = false;
+	BOOST_CHECK_EQUAL(args.get<char>("char1"), 'a');
+	BOOST_CHECK_EQUAL(args.get<char>("char2"), 'a');
+	BOOST_CHECK_EQUAL(args.get<char>("ic"), false);
+}
