2bb24da079c8848b738d1441f81b871d730d5d8b
Amorilia <amorilia@users.sourceforge.net>
2009-03-05 23:01:16 +0000

    cpp: more map hash algorithm tests

--- benchmark/attribute_access.cpp
+++ benchmark/attribute_access.cpp
@@ -8,10 +8,12 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <stdexcept>
 #include <boost/foreach.hpp>
 #include <boost/format.hpp>
 #include <boost/python.hpp>
-#if BOOST_VERSION >= 13600
+#include <boost/version.hpp>
+#if (BOOST_VERSION >= 103600)
 #include <boost/unordered/unordered_map.hpp>
 #endif
 
@@ -41,9 +43,61 @@ void impl_map(const std::vector<std::string> & names) {
 	};
 };
 
-#if BOOST_VERSION >= 13600
+#if (BOOST_VERSION >= 103600)
 void impl_hash_map(const std::vector<std::string> & names) {
 	boost::unordered_map<std::string, int> dict1;
+	for (int i=0; i < NUM_TRIALS; i++) {
+		BOOST_FOREACH(const std::string & name, names) {
+			dict1[name] = i;
+		};
+	};
+};
+#endif
+
+#if (BOOST_VERSION >= 103600)
+struct name_hash2 : std::unary_function<std::string, std::size_t> {
+	std::size_t operator()(std::string const & name) const {
+		std::size_t seed = 0;
+		try {
+			boost::hash_combine(seed, name.at(0));
+			boost::hash_combine(seed, name.at(2));
+			boost::hash_combine(seed, name.at(8));
+			//boost::hash_combine(seed, name.at(32));
+			//boost::hash_combine(seed, name.at(64));
+		} catch (const std::out_of_range &) {
+			// pass the exception
+		};
+		return seed;
+	}
+};
+
+void impl_hash_map2(const std::vector<std::string> & names) {
+	boost::unordered_map<std::string, int, name_hash2> dict1;
+	for (int i=0; i < NUM_TRIALS; i++) {
+		BOOST_FOREACH(const std::string & name, names) {
+			dict1[name] = i;
+		};
+	};
+};
+#endif
+
+#if (BOOST_VERSION >= 103600)
+struct name_hash3 : std::unary_function<std::string, std::size_t> {
+	std::size_t operator()(std::string const & name) const {
+		std::size_t seed = 0;
+		try {
+			 seed ^= name.at(0);
+			 seed ^= name.at(2);
+			 seed ^= name.at(8);
+		} catch (const std::out_of_range &) {
+			// pass the exception
+		};
+		return seed;
+	}
+};
+
+void impl_hash_map3(const std::vector<std::string> & names) {
+	boost::unordered_map<std::string, int, name_hash2> dict1;
 	for (int i=0; i < NUM_TRIALS; i++) {
 		BOOST_FOREACH(const std::string & name, names) {
 			dict1[name] = i;
@@ -88,10 +142,18 @@ int main(void) {
 	impl_map(names);
 	std::cout << "map: " << float(clock() - t) / CLOCKS_PER_SEC << " seconds"<< std::endl;
 
-#if BOOST_VERSION >= 13600
+#if (BOOST_VERSION >= 103600)
 	t = clock();
 	impl_hash_map(names);
 	std::cout << "hash map: " << float(clock() - t) / CLOCKS_PER_SEC << " seconds" << std::endl;
+
+	t = clock();
+	impl_hash_map2(names);
+	std::cout << "hash map 2: " << float(clock() - t) / CLOCKS_PER_SEC << " seconds" << std::endl;
+
+	t = clock();
+	impl_hash_map3(names);
+	std::cout << "hash map 3: " << float(clock() - t) / CLOCKS_PER_SEC << " seconds" << std::endl;
 #endif
 
 	t = clock();
