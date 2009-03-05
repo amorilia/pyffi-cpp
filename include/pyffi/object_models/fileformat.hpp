0b044a78e151dc93fa16d22aba557197f7d2dc9d
Amorilia <amorilia@users.sourceforge.net>
2009-03-05 11:04:12 +0000

    cpp: renamed a few things, Args is now ObjectMap (and is a wrapper around Map<Object>)

--- include/pyffi/object_models/fileformat.hpp
+++ include/pyffi/object_models/fileformat.hpp
@@ -35,41 +35,51 @@ POSSIBILITY OF SUCH DAMAGE.
 
 */
 
-#ifndef PYFFI_OM_ARGS_HPP_INCLUDED
-#define PYFFI_OM_ARGS_HPP_INCLUDED
+#ifndef PYFFI_OM_FORMAT_HPP_INCLUDED
+#define PYFFI_OM_FORMAT_HPP_INCLUDED
 
-#include <utility> // pair
+#include <vector>
+#include <map>
+#include <boost/shared_ptr.hpp>
 
-#include "pyffi/exceptions.hpp"
-#include "pyffi/object_models/map.hpp"
-#include "pyffi/object_models/object.hpp"
+#include "../exceptions.hpp"
+#include "meta_struct.hpp"
 
 namespace pyffi {
 
 namespace object_models {
 
-/*!
- * A std::map<std::string, Object> variant with an interface that
- * matches the rest of the library.
- */
-class Args {
+//! Stores all information attached to a format.
+class FileFormat {
 public:
-	//! Default constructor.
-	Args() : m_map() {};
-	//! Copy constructor.
-	Args(const Args & args) : m_map(args.m_map) {};
-	//! Constructor to set the type and the value.
-	template<typename ValueType> void add(const std::string & name, const ValueType & value) {
-		// if name not found, add object to the map
-		m_map.add(name, Object(value));
+	//! The name of the format (nif, cgf, tga, dds, ...)
+	static string name = "";
+
+	//! Add an attribute to the structure.
+	PMetaStruct add_struct(const std::string & name) {
+		// calculate index of new meta struct
+		// and store index
+		index_map[name] = meta_structs.size();
+		// create and store the meta struct
+		PMetaStruct meta_struct(new MetaStruct);
+		meta_structs.push_back(meta_struct);
+		return meta_struct;
 	};
-	//! Get reference to value stored in the object.
-	template<typename ValueType> ValueType & get(const std::string & name) {
-		return m_map.get(name).get<ValueType>();
-	};
+	PMetaStruct get_struct(const std::string & name) {
+		std::map<std::string, unsigned int>::const_iterator index
+		= index_map.find(name);
+		if (index != index_map.end()) {
+			return meta_structs[index->second];
+		} else {
+			throw name_error("Format has no struct \"" + name + "\".");
+		};
+	}
 private:
-	Map<Object> m_map;
-}; // class Args
+	//! Maps string name to their index as they have been added.
+	std::map<std::string, unsigned int> index_map;
+	//! List of meta structs as they have been added.
+	std::vector<PMetaStruct> meta_structs;
+}; // class Format
 
 }; // namespace object_models
 
