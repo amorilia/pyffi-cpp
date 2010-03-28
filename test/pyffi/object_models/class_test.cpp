1b1ce0c2dc9e848971e8278d529be91e1535148c
Amorilia <amorilia@users.sourceforge.net>
2010-03-28 21:18:49 +0100

    Some renames in class test for consistency.

--- test/pyffi/object_models/class_test.cpp
+++ test/pyffi/object_models/class_test.cpp
@@ -55,65 +55,65 @@ BOOST_AUTO_TEST_CASE(create_test)
 	//     class TestClass3
 	//     class TestClass4(TestClass3)
 	//     class TestClass5(TestClass1)
-	PClass ms0;
-	PClass ms1;
-	PClass ms2;
-	PClass ms3;
-	PClass ms4;
-	PClass ms5;
+	PClass scope;
+	PClass class1;
+	PClass class2;
+	PClass class3;
+	PClass class4;
+	PClass class5;
 
-	BOOST_CHECK_NO_THROW(ms0 = Class::scope());
-	BOOST_CHECK_NO_THROW(ms1 = ms0->class_("TestClass1"));
-	BOOST_CHECK_NO_THROW(ms2 = ms0->class_("TestClass2", ms1));
-	BOOST_CHECK_NO_THROW(ms3 = ms2->class_("TestClass3"));
-	BOOST_CHECK_NO_THROW(ms4 = ms2->class_("TestClass4", ms3));
-	BOOST_CHECK_NO_THROW(ms5 = ms2->class_("TestClass5", ms1));
+	BOOST_CHECK_NO_THROW(scope = Class::scope());
+	BOOST_CHECK_NO_THROW(class1 = scope->class_("TestClass1"));
+	BOOST_CHECK_NO_THROW(class2 = scope->class_("TestClass2", class1));
+	BOOST_CHECK_NO_THROW(class3 = class2->class_("TestClass3"));
+	BOOST_CHECK_NO_THROW(class4 = class2->class_("TestClass4", class3));
+	BOOST_CHECK_NO_THROW(class5 = class2->class_("TestClass5", class1));
 
 	// check that class cannot be added again
-	BOOST_CHECK_THROW(ms0->class_("TestClass1"), value_error);
-	BOOST_CHECK_THROW(ms0->class_("TestClass2"), value_error);
-	BOOST_CHECK_THROW(ms2->class_("TestClass3"), value_error);
-	BOOST_CHECK_THROW(ms2->class_("TestClass4"), value_error);
-	BOOST_CHECK_THROW(ms2->class_("TestClass5"), value_error);
+	BOOST_CHECK_THROW(scope->class_("TestClass1"), value_error);
+	BOOST_CHECK_THROW(scope->class_("TestClass2"), value_error);
+	BOOST_CHECK_THROW(class2->class_("TestClass3"), value_error);
+	BOOST_CHECK_THROW(class2->class_("TestClass4"), value_error);
+	BOOST_CHECK_THROW(class2->class_("TestClass5"), value_error);
 }
 
 BOOST_AUTO_TEST_CASE(def_test)
 {
-	PClass ms = Class::scope();
+	PClass scope = Class::scope();
 
 	// define attributes of various types
-	BOOST_CHECK_NO_THROW(ms->def("arg1", 5));
-	BOOST_CHECK_NO_THROW(ms->def("arg2", 'y'));
-	BOOST_CHECK_NO_THROW(ms->def("arg3", std::string("Hello world!")));
+	BOOST_CHECK_NO_THROW(scope->def("arg1", 5));
+	BOOST_CHECK_NO_THROW(scope->def("arg2", 'y'));
+	BOOST_CHECK_NO_THROW(scope->def("arg3", std::string("Hello world!")));
 
 	// check that attributes cannot be added again
-	BOOST_CHECK_THROW(ms->def("arg1", 0), value_error);
-	BOOST_CHECK_THROW(ms->def("arg2", 0), value_error);
-	BOOST_CHECK_THROW(ms->def("arg3", 0), value_error);
+	BOOST_CHECK_THROW(scope->def("arg1", 0), value_error);
+	BOOST_CHECK_THROW(scope->def("arg2", 0), value_error);
+	BOOST_CHECK_THROW(scope->def("arg3", 0), value_error);
 }
 
 BOOST_AUTO_TEST_CASE(attr_test)
 {
-	PClass ms0 = Class::scope();
-	PClass ms1 = ms0->class_("TestClass1");
-	PClass ms2 = ms0->class_("TestClass2", ms1);
-	ms1->def("arg1", 5);
-	ms2->def("arg2", 'y');
+	PClass scope = Class::scope();
+	PClass class1 = scope->class_("TestClass1");
+	PClass class2 = scope->class_("TestClass2", class1);
+	class1->def("arg1", 5);
+	class2->def("arg2", 'y');
 
 	// check if we get back the right classes
-	BOOST_CHECK_EQUAL(ms0->attr<PClass>("TestClass1"), ms1);
-	BOOST_CHECK_EQUAL(ms0->attr<PClass>("TestClass2"), ms2);
+	BOOST_CHECK_EQUAL(scope->attr<PClass>("TestClass1"), class1);
+	BOOST_CHECK_EQUAL(scope->attr<PClass>("TestClass2"), class2);
 
 	// check that we cannot get something that hasn't been added yet
-	BOOST_CHECK_THROW(ms0->attr<PClass>("TestClass3"), name_error);
+	BOOST_CHECK_THROW(scope->attr<PClass>("TestClass3"), name_error);
 
 	// check if we get back the right values
-	BOOST_CHECK_EQUAL(ms1->attr<int>("arg1"), 5);
-	BOOST_CHECK_EQUAL(ms2->attr<char>("arg2"), 'y');
+	BOOST_CHECK_EQUAL(class1->attr<int>("arg1"), 5);
+	BOOST_CHECK_EQUAL(class2->attr<char>("arg2"), 'y');
 
 	// check that we cannot get attributes that don't exist yet
-	BOOST_CHECK_THROW(ms1->attr<int>("arg3"), name_error);
-	BOOST_CHECK_THROW(ms2->attr<int>("arg3"), name_error);
+	BOOST_CHECK_THROW(class1->attr<int>("arg3"), name_error);
+	BOOST_CHECK_THROW(class2->attr<int>("arg3"), name_error);
 }
 
 BOOST_AUTO_TEST_CASE(issubclass_test)
@@ -123,33 +123,33 @@ BOOST_AUTO_TEST_CASE(issubclass_test)
 	// class TestClass2(TestClass1)
 	//     class TestClass3(TestClass1)
 	// class TestClass4(TestClass3)
-	PClass ms0 = Class::scope();
-	PClass ms1 = ms0->class_("TestClass1");
-	PClass ms2 = ms0->class_("TestClass2", ms1);
-	PClass ms3 = ms2->class_("TestClass3", ms1);
-	PClass ms4 = ms0->class_("TestClass4", ms3);
+	PClass scope = Class::scope();
+	PClass class1 = scope->class_("TestClass1");
+	PClass class2 = scope->class_("TestClass2", class1);
+	PClass class3 = class2->class_("TestClass3", class1);
+	PClass class4 = scope->class_("TestClass4", class3);
 
 	// check subclass relationship
-	BOOST_CHECK_EQUAL(ms0->issubclass(ms1), false);
-	BOOST_CHECK_EQUAL(ms0->issubclass(ms2), false);
-	BOOST_CHECK_EQUAL(ms0->issubclass(ms3), false);
-	BOOST_CHECK_EQUAL(ms0->issubclass(ms4), false);
-	BOOST_CHECK_EQUAL(ms1->issubclass(ms0), false);
-	BOOST_CHECK_EQUAL(ms1->issubclass(ms2), false);
-	BOOST_CHECK_EQUAL(ms1->issubclass(ms3), false);
-	BOOST_CHECK_EQUAL(ms1->issubclass(ms4), false);
-	BOOST_CHECK_EQUAL(ms2->issubclass(ms0), false);
-	BOOST_CHECK_EQUAL(ms2->issubclass(ms1), true);
-	BOOST_CHECK_EQUAL(ms2->issubclass(ms3), false);
-	BOOST_CHECK_EQUAL(ms2->issubclass(ms4), false);
-	BOOST_CHECK_EQUAL(ms3->issubclass(ms0), false);
-	BOOST_CHECK_EQUAL(ms3->issubclass(ms1), true);
-	BOOST_CHECK_EQUAL(ms3->issubclass(ms2), false);
-	BOOST_CHECK_EQUAL(ms3->issubclass(ms4), false);
-	BOOST_CHECK_EQUAL(ms4->issubclass(ms0), false);
-	BOOST_CHECK_EQUAL(ms4->issubclass(ms1), true);
-	BOOST_CHECK_EQUAL(ms4->issubclass(ms2), false);
-	BOOST_CHECK_EQUAL(ms4->issubclass(ms3), true);
+	BOOST_CHECK_EQUAL(scope->issubclass(class1), false);
+	BOOST_CHECK_EQUAL(scope->issubclass(class2), false);
+	BOOST_CHECK_EQUAL(scope->issubclass(class3), false);
+	BOOST_CHECK_EQUAL(scope->issubclass(class4), false);
+	BOOST_CHECK_EQUAL(class1->issubclass(scope), false);
+	BOOST_CHECK_EQUAL(class1->issubclass(class2), false);
+	BOOST_CHECK_EQUAL(class1->issubclass(class3), false);
+	BOOST_CHECK_EQUAL(class1->issubclass(class4), false);
+	BOOST_CHECK_EQUAL(class2->issubclass(scope), false);
+	BOOST_CHECK_EQUAL(class2->issubclass(class1), true);
+	BOOST_CHECK_EQUAL(class2->issubclass(class3), false);
+	BOOST_CHECK_EQUAL(class2->issubclass(class4), false);
+	BOOST_CHECK_EQUAL(class3->issubclass(scope), false);
+	BOOST_CHECK_EQUAL(class3->issubclass(class1), true);
+	BOOST_CHECK_EQUAL(class3->issubclass(class2), false);
+	BOOST_CHECK_EQUAL(class3->issubclass(class4), false);
+	BOOST_CHECK_EQUAL(class4->issubclass(scope), false);
+	BOOST_CHECK_EQUAL(class4->issubclass(class1), true);
+	BOOST_CHECK_EQUAL(class4->issubclass(class2), false);
+	BOOST_CHECK_EQUAL(class4->issubclass(class3), true);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
